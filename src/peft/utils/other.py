d5f0ae945a8f594b937eea7d7aaaceb742b272df
LRL <lrl@lbx.dev>
2024-12-04 23:11:10 +0800

    import hf_select_quant_linear

--- src/peft/utils/other.py
+++ src/peft/utils/other.py
@@ -648,6 +648,8 @@ def get_gptqmodel_quant_linear(gptq_quantization_config, device_map=None):
 
     if not is_gptqmodel_available():
         return None
+    
+    from gptqmodel.utils.importer import hf_select_quant_linear
 
     desc_act = gptq_quantization_config.desc_act
     group_size = gptq_quantization_config.group_size
